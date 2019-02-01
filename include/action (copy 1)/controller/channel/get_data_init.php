552ec0110205a5208ca5f1359ef527e9d4168e9f
nikolay <arinichev_n@mail.ru>
2019-02-01 04:37:36 +0300

    action function but not class added

--- include/action (copy 1)/controller/channel/get_data_init.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$af=function($p){
-	\sock\init($p['address'], $p['port']);
-	$acp_id=\acp\requestSendI1List(ACP_CMD_CHANNEL_GET_DATA_INIT, $p['item']);
-	$d = \acp\responseReadRows($acp_id);
-	\sock\suspend();
-	return $d;
-};
